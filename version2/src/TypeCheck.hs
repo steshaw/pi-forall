6c13efc3cfdf0d1e423b875d84c1edf74298a388
Steven Shaw <steven@steshaw.org>
2017-09-17 13:51:21 +1000

    Homework

--- version2/src/TypeCheck.hs
+++ version2/src/TypeCheck.hs
@@ -7,7 +7,11 @@
 {-# OPTIONS_GHC -Wall -fno-warn-unused-matches #-}
 
 -- | The main routines for type-checking
-module TypeCheck(tcModules, inferType, checkType) where
+module TypeCheck
+  ( tcModules
+  , inferType
+  , checkType
+  ) where
 
 import Syntax
 import Environment
@@ -20,7 +24,6 @@ import Unbound.Generics.LocallyNameless.Internal.Fold (toListOf)
 import Control.Monad.Except
 import Text.PrettyPrint.HughesPJ
 import Data.Maybe
-
 
 -- | Infer the type of a term, producing an annotated version of the
 -- term (whose type can *always* be inferred).
@@ -43,15 +46,15 @@ tcTerm :: Term -> Maybe Type -> TcMonad (Term,Type)
 
 tcTerm t@(Var x) Nothing = do
   ty  <- lookupTy x
-  return (t,ty)
+  pure (t,ty)
 
-tcTerm t@(Type) Nothing = return (t,Type)
+tcTerm t@(Type) Nothing = pure (t,Type)
 
 tcTerm (Pi bnd) Nothing = do
   ((x, unembed -> tyA), tyB) <- unbind bnd
   atyA <- tcType tyA
   atyB <- extendCtx (Sig x atyA) $ tcType tyB
-  return (Pi (bind (x, embed atyA) atyB), Type)
+  pure (Pi (bind (x, embed atyA) atyB), Type)
 
 -- Check the type of a function
 tcTerm (Lam bnd) (Just (Pi bnd2)) = do
@@ -59,10 +62,10 @@ tcTerm (Lam bnd) (Just (Pi bnd2)) = do
   ((x,unembed -> Annot ma), body,
    (_, unembed -> tyA), tyB) <- unbind2Plus bnd bnd2
   -- check tyA matches type annotation on binder, if present
-  maybe (return ()) (equate tyA) ma
+  maybe (pure ()) (equate tyA) ma
   -- check the type of the body of the lambda expression
   (ebody, etyB) <- extendCtx (Sig x tyA) (checkType body tyB)
-  return (Lam (bind (x, embed (Annot (Just tyA))) ebody),
+  pure (Lam (bind (x, embed (Annot (Just tyA))) ebody),
           Pi bnd2)
 tcTerm (Lam _) (Just nf) =
   err [DS "Lambda expression has a function type, not", DD nf]
@@ -71,28 +74,26 @@ tcTerm (Lam _) (Just nf) =
 -- on the binder is present
 tcTerm (Lam bnd) Nothing = do
   ((x,(unembed -> Annot annot)), body) <- unbind bnd
-  tyA  <- maybe (err [DS "Must annotate lambda"]) (return) annot
+  tyA  <- maybe (err [DS "Must annotate lambda"]) (pure) annot
   -- check that the type annotation is well-formed
   atyA <- tcType tyA
   -- infer the type of the body of the lambda expression
   (ebody, atyB) <- extendCtx (Sig x atyA) (inferType body)
-  return (Lam (bind (x, embed (Annot (Just atyA))) ebody),
-          Pi  (bind (x, embed atyA) atyB))
+  pure (Lam (bind (x, embed (Annot (Just atyA))) ebody),
+        Pi  (bind (x, embed atyA) atyB))
 
 tcTerm (App t1 t2) Nothing = do
   (at1, ty1)    <- inferType t1
   (x, tyA, tyB) <- ensurePi ty1
   (at2, ty2)    <- checkType t2 tyA
   let result = (App at1 at2, subst x at2 tyB)
-  return result
-
-
+  pure result
 
 tcTerm (Ann tm ty) Nothing = do
   ty'         <- tcType ty
   (tm', ty'') <- checkType tm ty'
 
-  return (tm', ty'')
+  pure (tm', ty'')
 
 tcTerm (Pos p tm) mTy =
   extendSourceLocation p tm $ tcTerm tm mTy
@@ -101,18 +102,15 @@ tcTerm (Paren tm) mTy = tcTerm tm mTy
 
 tcTerm t@(TrustMe ann1) ann2 = do
   expectedTy <- matchAnnots t ann1 ann2
-  return (TrustMe (Annot (Just expectedTy)), expectedTy)
+  pure (TrustMe (Annot (Just expectedTy)), expectedTy)
 
-tcTerm (TyUnit) Nothing = return (TyUnit, Type)
+tcTerm (TyUnit) Nothing = pure (TyUnit, Type)
 
-tcTerm (LitUnit) Nothing = return (LitUnit, TyUnit)
+tcTerm (LitUnit) Nothing = pure (LitUnit, TyUnit)
 
-tcTerm (TyBool) Nothing = return (TyBool,Type)
+tcTerm (TyBool) Nothing = pure (TyBool,Type)
 
-
-tcTerm (LitBool b) Nothing = do
-  return (LitBool b, TyBool)
-
+tcTerm (LitBool b) Nothing = pure (LitBool b, TyBool)
 
 tcTerm t@(If t1 t2 t3 ann1) ann2 = do
   ty <- matchAnnots t ann1 ann2
@@ -123,8 +121,7 @@ tcTerm t@(If t1 t2 t3 ann1) ann2 = do
         _     -> []
   (at2, _) <- extendCtxs (ctx True) $ checkType t2 ty
   (at3, _) <- extendCtxs (ctx False) $ checkType t3 ty
-  return (If at1 at2 at3 (Annot (Just ty)), ty)
-
+  pure (If at1 at2 at3 (Annot (Just ty)), ty)
 
 tcTerm (Let bnd) ann = do
   ((x,unembed->rhs),body) <- unbind bnd
@@ -133,19 +130,19 @@ tcTerm (Let bnd) ann = do
                 tcTerm body ann
   when (x `elem` toListOf fv ty) $
     err [DS "Let bound variable", DD x, DS "escapes in type", DD ty]
-  return (Let (bind (x,embed arhs) abody), ty)
+  pure (Let (bind (x,embed arhs) abody), ty)
 
 tcTerm (TyEq a b) Nothing =  do
   (aa,aTy) <- inferType a
   (ab,bTy) <- checkType b aTy
-  return (TyEq aa ab, Type)
+  pure (TyEq aa ab, Type)
 
 tcTerm t@(Refl ann1) ann2 =  do
   ty <- matchAnnots t ann1 ann2
   case ty of
     (TyEq a b) -> do
       equate a b
-      return (Refl (Annot (Just ty)), ty)
+      pure (Refl (Annot (Just ty)), ty)
     _ -> err [DS "refl annotated with", DD ty]
 
 tcTerm t@(Subst tm p ann1) ann2 =  do
@@ -159,18 +156,18 @@ tcTerm t@(Subst tm p ann1) ann2 =  do
     m'        <- whnf m
     n'        <- whnf n
     case (m',n') of
-        (Var x, _) -> return [Def x n']
-        (_, Var y) -> return [Def y m']
-        (_,_) -> return []
+        (Var x, _) -> pure [Def x n']
+        (_, Var y) -> pure [Def y m']
+        (_,_) -> pure []
 
   pdecl <- do
     p'        <- whnf apf
     case p' of
-      (Var x) -> return [Def x (Refl (Annot (Just tp)))]
-      _       -> return []
+      (Var x) -> pure [Def x (Refl (Annot (Just tp)))]
+      _       -> pure []
   let refined = extendCtxs (edecl ++ pdecl)
   (atm, _) <- refined $ checkType tm ty
-  return (Subst atm apf (Annot (Just ty)), ty)
+  pure (Subst atm apf (Annot (Just ty)), ty)
 
 tcTerm t@(Contra p ann1) ann2 = do
   ty <- matchAnnots t ann1 ann2
@@ -182,7 +179,7 @@ tcTerm t@(Contra p ann1) ann2 = do
 
 
     (LitBool b1, LitBool b2) | b1 /= b2 ->
-      return (Contra apf (Annot (Just ty)), ty)
+      pure (Contra apf (Annot (Just ty)), ty)
     (_,_) -> err [DS "I can't tell that", DD a, DS "and", DD b,
                   DS "are contradictory"]
 
@@ -191,7 +188,7 @@ tcTerm t@(Sigma bnd) Nothing = do
   ((x,unembed->tyA),tyB) <- unbind bnd
   aa <- tcType tyA
   ba <- extendCtx (Sig x aa) $ tcType tyB
-  return (Sigma (bind (x,embed aa) ba), Type)
+  pure (Sigma (bind (x,embed aa) ba), Type)
 
 
 tcTerm t@(Prod a b ann1) ann2 = do
@@ -201,7 +198,7 @@ tcTerm t@(Prod a b ann1) ann2 = do
       ((x, unembed-> tyA), tyB) <- unbind bnd
       (aa,_) <- checkType a tyA
       (ba,_) <- extendCtxs [Sig x tyA, Def x aa] $ checkType b tyB
-      return (Prod aa ba (Annot (Just ty)), ty)
+      pure (Prod aa ba (Annot (Just ty)), ty)
      _ -> err [DS "Products must have Sigma Type", DD ty,
                    DS "found instead"]
 
@@ -222,15 +219,14 @@ tcTerm t@(Pcase p bnd ann1) ann2 = do
             _     -> []
       (abody, bTy) <- extendCtxs ([Sig x' tyA, Sig y' tyB'] ++ ctx) $
         checkType body ty
-      return (Pcase apr (bind (x',y') abody) (Annot (Just ty)), bTy)
+      pure (Pcase apr (bind (x',y') abody) (Annot (Just ty)), bTy)
     _ -> err [DS "Scrutinee of pcase must have Sigma type"]
 
 
 tcTerm tm (Just ty) = do
   (atm, ty') <- inferType tm
   equate ty' ty
-
-  return (atm, ty)
+  pure (atm, ty)
 
 ---------------------------------------------------------------------
 -- helper functions for type checking
@@ -242,20 +238,20 @@ tcTerm tm (Just ty) = do
 matchAnnots :: Term -> Annot -> Maybe Type -> TcMonad Type
 matchAnnots e (Annot Nothing) Nothing     = err
  [DD e, DS "requires annotation"]
-matchAnnots e (Annot Nothing) (Just t)    = return t
+matchAnnots e (Annot Nothing) (Just t)    = pure t
 matchAnnots e (Annot (Just t)) Nothing    = do
   at <- tcType t
-  return at
+  pure at
 matchAnnots e (Annot (Just t1)) (Just t2) = do
   at1 <- tcType t1
   equate at1 t2
-  return at1
+  pure at1
 
 -- | Make sure that the term is a type (i.e. has type 'Type')
 tcType :: Term -> TcMonad Term
 tcType tm = do
   (atm, _) <- checkType tm Type
-  return atm
+  pure atm
 
 --------------------------------------------------------
 -- Using the typechecker for decls and modules and stuff
@@ -271,16 +267,16 @@ tcModules mods = foldM tcM [] mods
           let name = moduleName m
           liftIO $ putStrLn $ "Checking module " ++ show name
           m' <- defs `tcModule` m
-          return $ defs++[m']
+          pure $ defs++[m']
 
 -- | Typecheck an entire module.
 tcModule :: [Module]        -- ^ List of already checked modules (including their Decls).
          -> Module          -- ^ Module to check.
          -> TcMonad Module  -- ^ The same module with all Decls checked and elaborated.
 tcModule defs m' = do checkedEntries <- extendCtxMods importedModules $
-                                          foldr tcE (return [])
+                                          foldr tcE (pure [])
                                                   (moduleEntries m')
-                      return $ m' { moduleEntries = checkedEntries }
+                      pure $ m' { moduleEntries = checkedEntries }
   where d `tcE` m = do
           -- Extend the Env per the current Decl before checking
           -- subsequent Decls.
@@ -308,7 +304,7 @@ tcEntry (Def n term) = do
       lkup <- lookupHint n
       case lkup of
         Nothing -> do (aterm, ty) <- inferType term
-                      return $ AddCtx [Sig n ty, Def n aterm]
+                      pure $ AddCtx [Sig n ty, Def n aterm]
         Just ty ->
           let handler (Err ps msg) = throwError $ Err (ps) (msg $$ msg')
               msg' = disp [DS "When checking the term ", DD term,
@@ -318,9 +314,9 @@ tcEntry (Def n term) = do
                                checkType term ty `catchError` handler
             -- Put the elaborated version of term into the context.
             if (n `elem` toListOf fv eterm) then
-                 return $ AddCtx [Sig n ety, RecDef n eterm]
+                 pure $ AddCtx [Sig n ety, RecDef n eterm]
               else
-                 return $ AddCtx [Sig n ety, Def n eterm]
+                 pure $ AddCtx [Sig n ety, Def n eterm]
     die term' =
       extendSourceLocation (unPosFlaky term) term $
          err [DS "Multiple definitions of", DD n,
@@ -329,7 +325,7 @@ tcEntry (Def n term) = do
 tcEntry (Sig n ty) = do
   duplicateTypeBindingCheck n ty
   ety <- tcType ty
-  return $ AddHint (Hint n ety)
+  pure $ AddHint (Hint n ety)
 
 tcEntry _ = error "unimplemented"
 
@@ -342,7 +338,7 @@ duplicateTypeBindingCheck n ty = do
   l' <- lookupHint    n
   -- ... we don't care which, if either are Just.
   case catMaybes [l,l'] of
-    [] ->  return ()
+    [] ->  pure ()
     -- We already have a type in the environment so fail.
     ty':_ ->
       let (Pos p  _) = ty
